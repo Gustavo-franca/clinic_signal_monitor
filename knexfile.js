af8772846f72199edd872bb2d280433520151728
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-06-30 08:23:25 -0300

    configureted mysql

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
 
 const path = require('path');
+const {DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER} = require('./src/config/hostdatabase.json');
+
 
 module.exports = {
-    client: 'sqlite3',// banco utilizado -deve ser instalado o banco
-    connection:{
-        filename: path.resolve(__dirname,'src','database','database.sqlite'), //onde será armazenar o banco de dados
-    },
+    client: 'mysql',// banco utilizado -deve ser instalado o banco
+    connection: {
+        host : DB_HOST,
+        user : DB_USER,
+        password : DB_PASSWORD,
+        database : DB_NAME,
+        port:DB_PORT
+     },
     migrations:{
         directory: path.resolve(__dirname,'src','database','migrations'), //onde será as migrations
     },
